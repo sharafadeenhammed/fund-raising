8c51eec5123204c180721bc43946a881967c5fec
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2024-08-04 14:47:43 +0100

    modified:   src/component/OngoingItem.jsx

--- src/component/OngoingItem.jsx
+++ src/component/OngoingItem.jsx
@@ -8,16 +8,16 @@ function OngoingItem({ item }) {
       <OngoingItemModal handleClose={() => setOpenModal(false)} openModal={openModal} item={item} />
       < div className="bg-gray-100 h-full w-full rounded-2xl box-border" >
         <img
-          className="block border-none outline-none rounded-t-2xl h-[70%] w-full mb-5 object-cover"
+          className="block border-none outline-none rounded-t-2xl md:h-[70%] h-[58%] w-full mb-5 object-cover"
           src={item.image} />
         <div className="px-3 ">
           <p className="font-bold md:text-lg text-[14px]">
             {item.title}
           </p>
           <div
-            className=" text-red-950 my-5 flex justify-between items-center">
-            <p className="bg-gray-200 px-5 rounded-3xl py-3">Donate : {item.donationFrom}/{item.donationTo}</p>
-            <p onClick={() => setOpenModal(true)} className="hover:cursor-pointer text-white px-5 rounded-3xl py-3 bg-red-950">DONATE</p>
+            className=" text-red-950 my-5 flex md:flex-row flex-col justify-between items-center">
+            <p className=" md:w-auto w-full bg-gray-200 px-5 rounded-3xl py-3">Donated {item.donationFrom} of {item.donationTo}</p>
+            <p onClick={() => setOpenModal(true)} className=" my-3 text-center w-full md:w-auto  mx-1hover:cursor-pointer text-white px-5 rounded-3xl py-3 bg-red-950">DONATE</p>
           </div>
         </div>
       </div >
